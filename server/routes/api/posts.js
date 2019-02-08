3d16c21044b315026049c4b26a36970d4a92fcf2
Róbert Tóth <robert.toth.ext@nokia.com>
2019-02-08 15:05:33 +0100

    Post és delete

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -14,6 +14,32 @@ router.get('/', async (req, res) => {
     res.send(await posts.find({}).toArray()); //empty array because of find({})
 }) 
 
+
+//Add post
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text, //body-parser
+        createdAt: new Date()
+    });
+    //Return an HTTP response, 201 meaning OK and something was created
+    res.status(201).send();
+});
+
+
+//Delete request with specific ID
+// slash colon represents the ID
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    //Special field (ID), Object ID wrapper should be used
+    await posts.deleteOne({
+
+        _id : new mongoDb.ObjectID(req.params.id)
+    });
+    res.status(200).send();
+});
+
+
 //Funtion to connect to 'posts' collection
 // function in a route to do methods on it
 async function loadPostsCollection(){
